1baeeff507b1c83fbf7203d1ff37078a54fefedd
Crystak1831 <43097573+Crystak1831@users.noreply.github.com>
2019-10-15 19:04:28 -0400

    Add files via upload
    US11 and US 12

--- src/main/java/tools/userStories.java
+++ src/main/java/tools/userStories.java
@@ -10,6 +10,7 @@ package tools;
 
 import objects.Family;
 import objects.Individual;
+import org.jetbrains.annotations.NotNull;
 
 import java.text.ParseException;
 import java.util.*;
@@ -29,7 +30,7 @@ public class userStories {
     public Set<String> getError(){
         return this.ErrorInfo;
     }
-    
+
     public void IterateFam(Map _Fams, Map _indis) throws ParseException {
         Iterator<Map.Entry<String, Family>> entries1 = _Fams.entrySet().iterator();
         while (entries1.hasNext()) {
@@ -38,13 +39,10 @@ public class userStories {
             this.US05(curFam, _indis);
             this.US06(curFam,_indis);
             this.US04(curFam);
-//            this.US02(curFam,_indis);
+            this.US02(curFam,_indis);
             this.US08(curFam,_indis);
             this.US09(curFam,_indis);
-            this.US17(curFam,_indis);
-            this.US18(curFam,_indis);
-            this.US13(curFam,_indis);
-            this.US14(curFam,_indis);
+            this.US12(curFam, _indis);
         }
     }
 
@@ -56,6 +54,52 @@ public class userStories {
             this.US03(curIndis);
             this.US07(curIndis);
         }
+    }
+
+    // US12: Parents not too old(Ge Chang)
+    public String US12(Family _Fam, Map<String,Individual> _indis) {
+        String errStr = "";
+        Calendar calendar = Calendar.getInstance();
+        Individual husband = _indis.get(_Fam.getHusbandID());
+        calendar.setTime(husband.getBirthday());
+        calendar.add(Calendar.YEAR, 80);
+        Date fatherLast = calendar.getTime();
+
+        Individual wife = _indis.get(_Fam.getWifeID());
+        calendar.setTime(wife.getBirthday());
+        calendar.add(Calendar.YEAR, 60);
+        Date motherLast = calendar.getTime();
+
+        Iterator<String> itr = _Fam.getChildren().iterator();
+        while (itr.hasNext()) {
+            String curchild = itr.next();
+            Individual child = _indis.get(curchild);
+            if (child.getBirthday().after(fatherLast)) {
+                errStr = "ERROR: INDIVIDUAL: US12: "  + child.getId() + " is more than 80 years younger than Father: " + husband.getId();
+                this.ErrorInfo.add(errStr);
+            }
+
+            if (child.getBirthday().after(motherLast)) {
+                errStr = "ERROR: INDIVIDUAL: US12: "  + child.getId() + " is more than 60 years younger than Mother: " + wife.getId();
+                this.ErrorInfo.add(errStr);
+            }
+        }
+        return errStr;
+    }
+
+    // US11: No bigamy(Ge Chang)
+    public String US11(Map<String, Family> _Fams, Map<String, Individual> _indis) {
+        String errStr = "";
+        Iterator<Map.Entry<String, Individual>> entries = _indis.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<String, Individual> entry = entries.next();
+            Individual curIndi = entry.getValue();
+            if (curIndi.getSpouse().size() > 1) {
+                errStr = "ERROR: INDIVIDUAL: US11: "  + curIndi.getId() + " married in Family: " + curIndi.getSpouse();
+                this.ErrorInfo.add(errStr);
+            }
+        }
+        return errStr;
     }
 
     // US10: Marriage after 14(Jiaxian Xing)
@@ -191,6 +235,8 @@ public class userStories {
     }
 
 
+
+
     // US06: Divorce before death(Tao Chai)
     public String US06(Family _Fam, Map<String, Individual> _indis) throws ParseException {
         String errStr = "";
@@ -255,14 +301,15 @@ public class userStories {
         Individual husband = _indis.get(_Fam.getHusbandID());
         Individual wife = _indis.get(_Fam.getWifeID());
         if (husband.getBirthday().after(_Fam.getMarried())) {
-            errStr = "ERROR: FAMILY: US02: " +  _Fam.getId() + ": husband's birthday " + Formatdate.dateToString(husband.getBirthday()) + " after marriage " + Formatdate.dateToString(_Fam.getMarried());
+            errStr = "ERROR: FAMILY: US02: " + _Fam.getId() + ": husband's birthday " + Formatdate.dateToString(husband.getBirthday()) + " after marriage " + Formatdate.dateToString(_Fam.getMarried());
         }
-        if (wife.getBirthday().after(_Fam.getMarried())){
-            errStr = "ERROR: FAMILY: US02: " +  _Fam.getId() + ": wife's birthday " + Formatdate.dateToString(wife.getBirthday()) + " after marriage " + Formatdate.dateToString(_Fam.getMarried());
+        if (wife.getBirthday().after(_Fam.getMarried())) {
+            errStr = "ERROR: FAMILY: US02: " + _Fam.getId() + ": wife's birthday " + Formatdate.dateToString(wife.getBirthday()) + " after marriage " + Formatdate.dateToString(_Fam.getMarried());
         }
         this.ErrorInfo.add(errStr);
         return errStr;
     }
+
 
     // US01: Dates before current date(Jiaxian Xing)
     public String US01(Map _Fams, Map _indis) throws ParseException {
@@ -294,161 +341,6 @@ public class userStories {
                 if (curInd.getDeath().after(today))
                     errStr = "ERROR: INDIVIDUAL: US01: " + curInd.getId() + ": Death "+ Formatdate.dateToString(curInd.getDeath()) + " occurs in the future";
                 this.ErrorInfo.add("Error US01");
-            }
-        }
-        return errStr;
-    }
-
-    //    No marriages to children(Yining Wen)
-    public String US17(Family _Fam, Map<String,Individual> _indis) throws ParseException {
-        String errStr = "";
-        Set<String> children = _Fam.getChildren();
-        if(children != null){
-            for(String child : children) {
-                Individual childid = _indis.get(child);
-                Iterator<String> childspouse = childid.getSpouse().iterator(); // traversing over HashSet
-                while(childspouse.hasNext()) {
-                    String spouseFamId = childspouse.next();
-                    if (spouseFamId.equals(_Fam.getId())) {
-                        errStr = "ERROR: FAMILY: US17: Family: " + _Fam.getId() + "'s member " + childid.getName() + " married parent";
-                        this.ErrorInfo.add(errStr);
-                    }
-                }
-            }
-        }
-        return errStr;
-    }
-
-// Siblings should not marry(Yining Wen)
-    public String US18(Family _Fam, Map<String,Individual> _indis) throws ParseException {
-        String errStr = "";
-        Set<String> children = _Fam.getChildren();
-        Set<String> husbandchildfamID = _indis.get(_Fam.getHusbandID()).getChild();
-        Set<String> wifechildfamID = _indis.get(_Fam.getWifeID()).getChild();
-        Set<String> allspouse = new HashSet<>();
-
-        for(String childID: children){
-            for(String eachSpouseFamID: _indis.get(childID).getSpouse()){
-                if(allspouse.contains(eachSpouseFamID)){
-                    errStr = "ERROR: FAMILY: US18: "+" FamilyID: "+_Fam.getId()+" has two children that married each other" ;
-                    this.ErrorInfo.add(errStr);
-                }else{
-                    allspouse.add(eachSpouseFamID);
-                }
-            }
-        }
-
-//        if(children != null){
-//            for(String child : children) {
-//                Individual childid = _indis.get(child);
-//                Iterator<String> childspouse = childid.getSpouse().iterator(); // traversing over HashSet
-//                while(childspouse.hasNext()) {
-//                    allspouse.add(childspouse)
-//                        if (allspouse.contains(husbandchildfamID) && allspouse.contains(wifechildfamID)) {
-//                            errStr = "ERROR: FAMILY: US18: Family: " + _Fam.getId() + "'s member " + childid.getName() + " married parent";
-//                            this.ErrorInfo.add(errStr);
-//                        }
-//
-//
-//                }
-//            }
-//        }
-        return errStr;
-    }
-//    public String US18(Family _Fam, Map<String, Individual> _indis) throws ParseException{
-//        String errStr = "";
-//        Set<String> husbandFamID = _indis.get(_Fam.getHusbandID()).getChild();
-//        Set<String> wifeFamID = _indis.get(_Fam.getWifeID()).getChild();
-//        String husbandFatherFamID= "",husbandMotherFamID = "";
-//        String wifeFatherFamID= "",wifeMotherFamID = "";
-////        Set<String> husbandFatherFamID, husbandMotherFamID;
-//        if(husbandFamID != null && wifeFamID != null) {
-//            for (String hfi : husbandFamID) {
-//                for (String wfi : wifeFamID) {
-//                    if (_Fam.getId().equals(hfi)) {
-//                        husbandFatherFamID = hfi;
-//                        husbandMotherFamID = wfi;
-//                    } else if (_Fam.getId().equals(wfi)) {
-//                        wifeFatherFamID = hfi;
-//                        wifeMotherFamID = wfi;
-//                    }
-//                }
-//                if(husbandFatherFamID.equals(wifeFatherFamID) && husbandMotherFamID.equals(wifeMotherFamID)){
-//                    errStr = "ERROR: FAMILY: US18: The husband is from family" + husbandFatherFamID + "the wife is from family" + wifeMotherFamID + "are the children from a same family.";
-//                    this.ErrorInfo.add(errStr);
-//                }
-//            }
-//
-//        }
-//        return errStr;
-//    }
-
-    //US13 Birth dates of siblings should be more than 8 months apart or less than 2 days apart (twins may be born one day apart, e.g. 11:59 PM and 12:02 AM the following calendar day)
-    public String US13(Family _Fam, Map<String, Individual> _indis){
-        String errStr = "";
-        Map<String, Date> siblings = new HashMap<>();
-        //put every child <id,birthday> in HashMap;
-        for(String siblingId: _Fam.getChildren()){
-            Individual curInd = _indis.get(siblingId);
-            Date curIndBd = curInd.getBirthday();
-            siblings.put(siblingId, curIndBd);
-        }
-        //iterate through every pair of child.
-        for(String siblingId1: _Fam.getChildren()){
-            Date SiblingBd1 = siblings.get(siblingId1);
-            for(String siblingId2: _Fam.getChildren()) {//TODO:should start from siblingId1 to end!
-                Date SiblingBd2 = siblings.get(siblingId2);
-
-                if(SiblingBd1.before(SiblingBd2)){
-                    Date temp = SiblingBd1;
-                    SiblingBd1 = SiblingBd2;
-                    SiblingBd2= temp;
-                }
-                long diff = SiblingBd1.getTime() - SiblingBd2.getTime();
-                int diffDays = (int) (diff / (24 * 60 * 60 * 1000));
-                int diffMonth = (int) (diffDays / 30);
-                int diffYear = (int) (diffMonth / 12);
-                if(diffYear == 0){
-                    if (diffMonth < 8) {
-                        if (diffMonth == 0) {
-                            if (diffDays > 2)
-                                errStr = "ERROR: US13: FamilyID:"+_Fam .getId()+" children:"+siblingId1+" and children:"+siblingId2+"birthday difference greater than 2 days AND less than 8 months ";
-                                this.ErrorInfo.add(errStr);
-                        }
-                        else {
-                            errStr = "ERROR: US13: FamilyID:"+_Fam .getId()+" children:"+siblingId1+" and children:"+siblingId2+"birthday difference less than 8 months AND greater than 2 days";
-                            this.ErrorInfo.add(errStr);
-                        }
-                    }
-                }
-            }
-        }
-        return errStr;
-    }
-
-    //US14 No more than five siblings should be born at the same time
-    public String US14(Family _Fam, Map<String, Individual> _indis) throws ParseException {
-        String errStr = "";
-        //put every child <birthday> in HashSet;
-        Map<Date,Integer> siblings = new HashMap<>();
-        for(String siblingId: _Fam.getChildren()){
-            Individual curInd = _indis.get(siblingId);
-            Date curIndBd = curInd.getBirthday();
-            if(siblings.containsKey(curIndBd)){
-                int count = siblings.get(curIndBd);
-                siblings.replace(curIndBd,count+1);
-            }
-            else {
-                siblings.put(curIndBd,1);
-            }
-        }
-        //iterate through every child.
-        for (Map.Entry<Date, Integer> entry : siblings.entrySet()) {
-            Date birthday = entry.getKey();
-            Integer count = entry.getValue();
-            if(count > 5){
-                errStr = "ERROR: US14: FamilyID:"+_Fam .getId()+"has more than five child is born"+" in date:"+Formatdate.dateToString(birthday);
-                this.ErrorInfo.add(errStr);
             }
         }
         return errStr;
