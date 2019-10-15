1b15d3caf22d5d8375a048f8b7e49c7645dfb58e
xian96 <42896816+xian96@users.noreply.github.com>
2019-10-15 17:42:27 -0400

    fix us14 count problem

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -24,8 +24,9 @@ public class main {
 //        read.readFile("src\\main\\resources\\My-Family-17-Sep-2019-579.ged");
 //        read.readFile("src\\main\\resources\\TestFamilyTreeWithmanyissues.ged");
 //        read.readFile("src\\main\\resources\\TestGEDCOM.ged");
-//        read.readFile("src\\main\\resources\\testOne.ged");
-        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/UserStory17.ged");
+        read.readFile("src\\main\\resources\\testOne.ged");
+//        read.readFile("src\\main\\resources\\testUs1314.ged");
+//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/UserStory17.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/My-Family-17-Sep-2019-579.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testOne.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/US18.ged");
