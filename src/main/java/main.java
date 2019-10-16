2c07182bc2a88d8cca8bab4440ff8ccf389d83fa
MicahelWen318 <wen820061663@gmail.com>
2019-10-15 20:23:26 -0400

    test

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -44,5 +44,6 @@ public class main {
         System.out.println(test.US01(Fams, indis));
         System.out.println(test.US10(Fams, indis));
         System.out.println(test.US11(Fams, indis));
+//        testconflict
     }
 }
