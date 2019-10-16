f2db911dac3a712dd1e5c67e1ad85400a77807c4
MicahelWen318 <wen820061663@gmail.com>
2019-10-15 20:17:42 -0400

    addreport

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -29,8 +29,8 @@ public class main {
 //        read.readFile("src\\main\\resources\\testUs1314.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/UserStory17.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/My-Family-17-Sep-2019-579.ged");
-//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testOne.ged");
-        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/US18.ged");
+        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testOne.ged");
+//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/US18.ged");
         Map indis = read.printIndi();
         Map Fams = read.printFam();
         userStories test = new userStories();
