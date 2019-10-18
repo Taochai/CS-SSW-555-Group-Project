db08f98077b8fb7917d75a56f85d14b8303e9500
MicahelWen318 <wen820061663@gmail.com>
2019-10-18 12:43:02 -0400

    add all

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -27,12 +27,12 @@ public class main {
 //        read.readFile("src\\main\\resources\\testTwo.ged");
 //        read.readFile("src\\main\\resources\\testOne.ged");
 //        read.readFile("src\\main\\resources\\testUs1314.ged");
-//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/TestFamilyTreeWithmanyissues.ged");
+        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/TestFamilyTreeWithmanyissues.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/TestGEDCOM.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/UserStory17.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testOne.ged");
 //        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/US18.ged");
-        read.readFile(("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testTwo.ged"));
+//        read.readFile(("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testTwo.ged"));
 //        read.readFile("src\\main\\resources\\testOne.ged");
         Map indis = read.printIndi();
         Map Fams = read.printFam();
