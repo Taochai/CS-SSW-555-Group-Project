d71e915a4ca4a40ac9dc2e6a092722d7676d74bf
Taochai <42437872+Taochai@users.noreply.github.com>
2019-10-18 11:03:50 -0400

    outputTXT

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,45 +7,40 @@
  * @LastEditTime: 2019-09-30 13:18:32
  */
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.Map;
 import tools.readGedcomFile;
 import tools.userStories;
-
+import tools.outputTxt;
 
 /**
  * main function
  */
 public class main {
     public static void main(String[] args) throws Exception {
+       String outputStr = "";
+       outputStr = readTestFile("src\\main\\resources\\testOne.ged",outputStr);
+       outputStr = readTestFile("src\\main\\resources\\testUS15.ged",outputStr);
+        outputTxt text = new outputTxt();
+        text.log(outputStr);
+    }
+    public static String readTestFile(String _testFileName, String _errorStr) throws Exception {
         readGedcomFile read = new readGedcomFile();
-        // C:\Users\jason\Documents\Git\SSW-CS-555-Group-project\CS555\My-Family-17-Sep-2019-579.ged
-        // G:\Courses Info\SSW 555 Agile Dev\Sprint1\SSW-CS-555-Group-project\CS555\My-Family-17-Sep-2019-579.ged
-
-//        read.readFile("src\\main\\resources\\My-Family-17-Sep-2019-579.ged");
-//        read.readFile("src\\main\\resources\\TestFamilyTreeWithmanyissues.ged");
-//        read.readFile("src\\main\\resources\\TestGEDCOM.ged");
-//        read.readFile("src\\main\\resources\\testTwo.ged");
-//        read.readFile("src\\main\\resources\\testOne.ged");
-//        read.readFile("src\\main\\resources\\testUs1314.ged");
-//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/UserStory17.ged");
-//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/My-Family-17-Sep-2019-579.ged");
-//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testOne.ged");
-//        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/US18.ged");
-        read.readFile("/Users/michaelwen/Documents/555/homework/CS-SSW-555-Group-Project/src/main/resources/testOne.ged");
-//        read.readFile("src\\main\\resources\\testOne.ged");
+        read.readFile(_testFileName);
         Map indis = read.printIndi();
         Map Fams = read.printFam();
         userStories test = new userStories();
         test.IterateFam(Fams,indis);
         test.IterateInds(Fams,indis);
+
+        test.US01(Fams, indis);
+        test.US10(Fams, indis);
+        test.US11(Fams, indis);
+//        String out = "";
         for (String a : test.getError()) {
-            System.out.println(a);
+            _errorStr +=  a+"\n";
         }
-        System.out.println(test.US01(Fams, indis));
-        System.out.println(test.US10(Fams, indis));
-        System.out.println(test.US11(Fams, indis));
-
-        //********TESTconflicts
-
+        return _errorStr;
     }
 }
---
+++ src/main/java/tools/outputTxt.java
@@ -0,0 +1,13 @@
+package tools;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class outputTxt {
+    public static void log(String message) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter("output.txt", true), true);
+        out.write(message);
+        out.close();
+    }
+}
