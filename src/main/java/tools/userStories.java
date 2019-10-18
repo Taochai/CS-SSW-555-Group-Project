087762f7b931c85dabe2cf07b8a6da7ffcb154aa
Taochai <42437872+Taochai@users.noreply.github.com>
2019-10-18 11:46:58 -0400

    sprint2

--- src/main/java/tools/userStories.java
+++ src/main/java/tools/userStories.java
@@ -74,12 +74,12 @@ public class userStories {
             if(curFam.getDivorced() != null){
                 if(curFam.getDivorced().after(today))
                     errStr = "ERROR: FAMILY: US01: " + curFam.getId() +": Divorced date " + Formatdate.dateToString(curFam.getDivorced()) + " occurs in the future";
-                this.ErrorInfo.add("Error US01");
+                this.ErrorInfo.add(errStr);
             }
             if(curFam.getMarried() != null){
                 if(curFam.getMarried().after(today))
                     errStr = "ERROR: FAMILY: US01: " + curFam.getId() +": Married date " + Formatdate.dateToString(curFam.getMarried()) + " occurs in the future";
-                this.ErrorInfo.add("Error US01");
+                this.ErrorInfo.add(errStr);
             }
         }
         for (Map.Entry<String, Individual> entry : (Iterable<Map.Entry<String, Individual>>) _indis.entrySet()) {
@@ -87,12 +87,12 @@ public class userStories {
             if (curInd.getBirthday() != null) {
                 if (curInd.getBirthday().after(today))
                     errStr = "ERROR: INDIVIDUAL: US01: " + curInd.getId() + ": Birthday "+ Formatdate.dateToString(curInd.getBirthday()) + " occurs in the future";
-                this.ErrorInfo.add("Error US01");
+                this.ErrorInfo.add(errStr);
             }
             if (curInd.getDeath() != null) {
                 if (curInd.getDeath().after(today))
                     errStr = "ERROR: INDIVIDUAL: US01: " + curInd.getId() + ": Death "+ Formatdate.dateToString(curInd.getDeath()) + " occurs in the future";
-                this.ErrorInfo.add("Error US01");
+                this.ErrorInfo.add(errStr);
             }
         }
         return errStr;
